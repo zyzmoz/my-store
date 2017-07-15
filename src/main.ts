d2e08b6547f3ad6b400d700eeaa393e3751502ed
Daniel Brandão <danielbrandaodacunha@gmail.com>
2017-07-15 15:18:54 -0300

    SW done

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,27 @@ platformBrowserDynamic()
   .then(() => {
     //Register Service worker
     if ('serviceWorker' in navigator){
-      navigator.serviceWorker.register('/sw.js');
+      navigator.serviceWorker.register('/sw.js').then(reg => {
+        reg.onupdatefound = function(){
+          const installingWorker = reg.installing;
+          installingWorker.onstatechange = function(){
+            switch (installingWorker.state) {
+              case 'installed':
+                if (navigator.serviceWorker.controller){
+                  console.log('New contant or update available');                  
+                } else {
+                  console.log('Content is now available offline');                  
+                }
+                break;
+
+              case 'redundant':
+                console.error('The current installing worker became redundant');
+            }
+          }
+        }
+      }).catch(e =>{
+        console.error(e);
+      });
     }
 
   });
