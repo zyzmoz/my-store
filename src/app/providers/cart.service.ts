e6ce5bd1e9af80a2f055093c61ef10f08d21ad8f
Daniel Cunha <danielbrandaodacunha@gmail.com>
2018-02-26 10:53:58 -0300

    Update

--- src/app/providers/cart.service.ts
+++ src/app/providers/cart.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../models/cart-item';
-import * as pagseguro from 'pagseguro';
 
 @Injectable()
 export class CartService {
@@ -44,60 +43,10 @@ export class CartService {
 
   checkout(){
     this.wait = true;
-    let pag = new pagseguro({
-      email: 'suporte@lojamodelo.com.br',
-      token: '57BE455F4EC148E5A54D9BB91C5AC12C'
-    });
-    let obj = {
-      bornDate: '18/09/1991',      
-      areaCode: '19',
-      number: '988033320',      
-      email: 'zyzmoz@sandbox.pagseguro.com.br',
-      document: {
-        type: 'CPF',
-        value: '37126058896'
-      },
-      name: 'Daniel Cunha',
-      ip: '1.1.1.1'
-    }
-    pag.setSender(obj);
-    pag.addItens(this.cartList);
-    /**address: {
-                        street: obj.street,
-                        number: obj.number,
-                        complement: obj.complement,
-                        district: obj.district,
-                        city: obj.city,
-                        state: obj.state,
-                        country: obj.country,
-                        postalCode: obj.postalCode
-                    },
-                    type: 1,
-                    cost: obj.cost, */
-    pag.setShippmentAddress({street: 'Rua Maria M de Souza Campos', number: 58, complement: '',
-                             district: 'Jd Alto das Araras', city: 'Araras', state: 'SP', country: 'Brasil',
-                             postalCode: '13604120' });
-    pag.setPaymentMethod('boleto', {});
-    pag.checkout((err, res) => {
-      if (err){
-        console.log(err);
-        
-      } else {
-        console.log(res);       
-      }
+    setTimeout(() => {
+      this.cartList = [];
       this.wait = false;
-      this.cartList.splice(0,this.cartList.length);
-      this.paymentDone = true; 
-      setTimeout(() =>{
-        this.paymentDone = false;
-        console.log('False');
-        
-      },3000);     
-
-    });
-
-    
-    
+    },3000);    
 
   }
 
